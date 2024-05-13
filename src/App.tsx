86a590634978a5813490d6f946fc7d751117d092
voidrak <112881704+voidrak@users.noreply.github.com>
2024-05-13 23:01:24 +0300

    add feature section and import new font

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import HeroSection from "./Components/HeroSection/HeroSection";
+import Features from "./Components/Features/Features";
 
 const App = () => {
   return (
-    <div className="font-ClashGrotesk  ">
+    <div className="font-ClashGrotesk  mb-[100vw] ">
       <HeroSection />
+      <Features />
     </div>
   );
 };
---
+++ src/Components/Features/Features.tsx
@@ -0,0 +1,18 @@
+const Features = () => {
+  return (
+    <div className="py-16 px-4">
+      <div className="">
+        <h1 className="font-bold pt-8 pb-16 text-5xl">FEATURE</h1>
+        <h1 className=" travel text-7xl">We make travel Easier</h1>
+      </div>
+      <div className="py-8 ">
+        <h1 className="">
+          We provide you with the best possible way to make it easier for you to
+          travel to ethiopia and enjoy your tour
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Features;
