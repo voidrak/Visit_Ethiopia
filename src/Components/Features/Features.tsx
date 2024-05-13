4ebaefda571b0f59518ed1bc3d93d29b14c8e9f9
voidrak <112881704+voidrak@users.noreply.github.com>
2024-05-13 23:16:43 +0300

    finished the features page

--- src/Components/Features/Features.tsx
+++ src/Components/Features/Features.tsx
@@ -1,15 +1,21 @@
 const Features = () => {
   return (
-    <div className="py-16 px-4">
-      <div className="">
-        <h1 className="font-bold pt-8 pb-16 text-5xl">FEATURE</h1>
-        <h1 className=" travel text-7xl">We make travel Easier</h1>
-      </div>
-      <div className="py-8 ">
-        <h1 className="">
-          We provide you with the best possible way to make it easier for you to
-          travel to ethiopia and enjoy your tour
-        </h1>
+    <div className="py-16 px-4 before lg:px-6 container mx-auto">
+      <h1 className="font-bold pt-8  xl:pb-24 2xl:pb-32 pb-16 text-2xl">
+        FEATURE
+      </h1>
+      <div className="sm:flex   justify-between md:gap-x-4 ">
+        <div className="sm:max-w-[60%]">
+          <h1 className=" travel text-7xl md:text-8xl xl:text-9xl">
+            We make travel Easier
+          </h1>
+        </div>
+        <div className="py-8 sm:max-w-[30%] ">
+          <h1 className="font-light text-gray-300 text-lg sm:text-2xl ">
+            We provide you with the best possible way to make it easier for you
+            to travel to ethiopia and enjoy your tour
+          </h1>
+        </div>
       </div>
     </div>
   );
