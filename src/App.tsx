6107835cae637b0214720b417ae961e6f5d7db8c
voidrak <112881704+voidrak@users.noreply.github.com>
2024-05-14 19:46:46 +0300

    add animation on hero page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import HeroSection from "./Components/HeroSection/HeroSection";
 import Features from "./Components/Features/Features";
 
 const App = () => {
+  const [isLoad, setLoad] = useState<boolean>(false);
+  useEffect(() => {
+    console.log(isLoad);
+  }, [isLoad]);
   return (
     <div className="font-ClashGrotesk  mb-[100vw] ">
-      <HeroSection />
-      <Features />
+      <img
+        onLoad={() => {
+          setLoad(true);
+        }}
+        src="public/images/BG-IMG.png"
+        alt=""
+        className="absolute top-0 left-0 opacity-0"
+      />
+      isLoad?( : <div className="loader"></div> ):(
+      <HeroSection isLoad={isLoad} setLoad={setLoad} />
+      <Features />)
     </div>
   );
 };
-
 export default App;
--- src/Components/HeroSection/HeroSection.tsx
+++ src/Components/HeroSection/HeroSection.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HeadingNav from "./HeadingNav/HeadingNav";
 import MainHero from "./MainHero";
 
-const HeroSection = () => {
+interface Props {
+  isLoad: boolean;
+  setLoad: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const HeroSection = ({ isLoad, setLoad }: Props) => {
+  // useEffect(() => {
+  //   console.log(isLoad);
+  //   setLoad(true);
+  // }, []);
   return (
     <div className=" relative  h-screen bg-[url('images/BG-IMG.png')]  bg-no-repeat bg-cover   flex flex-col items-center justify-center  ">
       <HeadingNav />
