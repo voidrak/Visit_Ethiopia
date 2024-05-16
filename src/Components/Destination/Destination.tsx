d8b5411ca855fe0cb9640f96ccb170cac1d6d42b
voidrak <112881704+voidrak@users.noreply.github.com>
2024-05-17 00:26:32 +0300

    add tab indicator

--- src/Components/Destination/Destination.tsx
+++ src/Components/Destination/Destination.tsx
@@ -13,8 +13,6 @@ const Destination = () => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [activeButton, setActiveButton] = useState<string>("All");
   const windowSize = window.innerWidth;
-  console.log(windowSize);
-  // const indexMul = window.innerWidth < 1330 ? 94 : 90;
   let indexMul: number;
   if (windowSize <= 1200) {
     indexMul = 94;
@@ -48,7 +46,7 @@ const Destination = () => {
 
   const mappedItems = destinations.map((card: data) => (
     <div
-      className="slider-card relative  mx-auto rounded-[30px] flex flex-col h-[555px] justify-end min-w-[300px] min-[373px]:min-w-[355px] max-w-[356px]  after:absolute  after:bg-gradient-to-b from-transparent to-[#161b23] after:w-full after:h-full after:top-0 after:left-0 after:-z-10 after:rounded-[30px] border border-gray-700 "
+      className="slider-card relative  mx-auto rounded-[30px] transition-all duration-300 ease-linear flex flex-col h-[555px] justify-end min-w-[300px] min-[373px]:min-w-[355px] max-w-[356px]  after:absolute  after:bg-gradient-to-b from-transparent to-[#161b23] after:w-full after:h-full after:top-0 after:left-0 after:-z-10 after:rounded-[30px] border border-gray-700 "
       key={card.name}
       style={{
         transform: `translate(-${currentIndex * indexMul}%)`,
@@ -79,28 +77,38 @@ const Destination = () => {
       </button>
     </div>
   ));
-  // console.log(mappedItems);
+  console.log(activeButton);
   return (
     <div className="mt-16    mx-auto">
-      <div className="controls-btn  px-4  flex justify-between items-center">
+      <div className="controls-btn lg:px-16 px-4  2xl:px-[6rem]  flex justify-between items-center">
         <div className="filter-btn space-x-8">
           <button
-            className={activeButton === "All" ? "active-btn" : ""}
+            className={`${
+              activeButton === "All" ? "underline decoration-gold-accent" : " "
+            }`}
             onClick={() => {
               setDestinations(destinationData);
               setActiveButton("All");
             }}
           >
-            <h3 className="font-semibold text-xl">All</h3>
+            <h3 className="font-semibold text-xl lg:text-2xl xl:text-3xl  ">
+              All
+            </h3>
           </button>
           <button
-            className={activeButton === "Nearby" ? "active-btn" : ""}
+            className={`${
+              activeButton === "Nearby"
+                ? "underline decoration-gold-accent"
+                : " "
+            }`}
             onClick={() => {
               filterItems("Near by");
               setActiveButton("Nearby");
             }}
           >
-            <h3 className="font-semibold text-xl">Near By</h3>
+            <h3 className="font-semibold text-xl lg:text-2xl xl:text-3xl   ">
+              Near By
+            </h3>
           </button>
         </div>
 
@@ -109,7 +117,7 @@ const Destination = () => {
             <img
               src="images/pervious_arrow.png"
               alt="pervious arrow"
-              className="pervious-arrow  w-[50px]"
+              className="pervious-arrow  lg:w-[70px] 2xl:w-[85px] w-[50px]"
               onClick={() => {
                 updateIndex(currentIndex - 1);
               }}
@@ -119,7 +127,7 @@ const Destination = () => {
             <img
               src="images/next_arrow.png"
               alt="next arrow"
-              className="next-arrow w-[50px]"
+              className="next-arrow lg:w-[70px] 2xl:w-[85px] w-[50px]"
               onClick={() => {
                 updateIndex(currentIndex + 1);
               }}
@@ -127,7 +135,7 @@ const Destination = () => {
           </button>
         </div>
       </div>
-      <h1 className="card-title mt-8 mb-8 ml-2 font-extrabold tracking-widest text-3xl">
+      <h1 className="card-title mt-8 mb-8 ml-2 lg:px-16 2xl:px-[6rem]  font-extrabold tracking-widest text-3xl xl:text-4xl">
         Popular Destination
       </h1>
       <div className=" grid md:grid-cols-2 gap-x-4 lg:flex lg:ml-16  overflow-x-hidden gap-y-8 ">
