7b0bcd84f9b67925948fa7ef178a59de4ea6f90d
voidrak <112881704+voidrak@users.noreply.github.com>
2024-05-19 01:17:15 +0300

    update MobileNav.tsx

--- src/Components/HeroSection/HeadingNav/MobileNav.tsx
+++ src/Components/HeroSection/HeadingNav/MobileNav.tsx
@@ -67,8 +67,6 @@ const MobileNav = () => {
   return (
     <div
       ref={navRef}
-      //   onMouseEnter={handleMouseEnter}
-      //   onMouseLeave={handleMouseLeave}
       className=" fixed right-0 top-0 z-50 overscroll-contain min-[850px]:hidden   "
     >
       <div
